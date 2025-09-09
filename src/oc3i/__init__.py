bba7ade151fcd67267c151231cb743cf45885c81
Jeroen Minderman <Jeroen.Minderman@ons.gov.uk>
2025-09-09 16:28:59 +0100

    Edit to version tracking to avoid sourcing from pyproject.toml, and to fix plain package install

--- src/oc3i/__init__.py
+++ src/oc3i/__init__.py
@@ -1,9 +1,2 @@
 # src/oc3i/__init__.py
-from pathlib import Path
-import tomllib  # built-in in Python 3.11+
-
-pyproject_file = Path(__file__).resolve().parents[2] / "pyproject.toml"
-with pyproject_file.open("rb") as f:
-    pyproject_data = tomllib.load(f)
-
-__version__ = pyproject_data["project"]["version"]
+__version__ = "0.1.0"
